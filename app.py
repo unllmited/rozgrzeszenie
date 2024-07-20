3e51db994f0353d3c5c6efcd11705f61a4bb5800
Paweł Cejrowski <pcejrowski@gmail.com>
2024-07-20 12:08:57 +0200

    .

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import random
+
 import streamlit as st
 from dotenv import load_dotenv
 
@@ -8,32 +10,52 @@ load_dotenv()
 
 st.set_page_config(
     page_title="Rozgrzeszenie Online",
-    page_icon=":memo:",
+    page_icon=":cross:",
     layout="wide",
     menu_items={
         "Get Help": "mailto:pawel+rozgrzeszenie@cejrowski.biz",
         "Report a bug": "mailto:pawel+rozgrzeszenie@cejrowski.biz",
     },
 )
+
 st.title('Automatyczna Spowiedź')
 
 st.divider()
 
 with st.form("confession_form"):
+    st.subheader("Wstęp")
     st.write("Niech będzie pochwalony Jezus Chrystus. Na wieki wieków. Amen")
     st.write("W imię Ojca i Syna, i Ducha Świętego. Amen.")
-    last_confession = st.text_input("Ostatni raz u spowiedzi świętej byłem:")
+
+    st.subheader("Ostatnia spowiedź")
+    last_confession = st.date_input("Ostatni raz u spowiedzi świętej byłem:")
+
+    st.subheader("Grzechy")
     sins = st.text_area("Obraziłem Pana Boga następującymi grzechami:")
-    submit_button = st.form_submit_button("Wyślij Spowiedź")
 
-    callback_handler = StreamHandler(container=st.empty())
+    submit_button = st.form_submit_button("Wyślij spowiedź")
 
-    params = {
-        "last_confession": last_confession,
-        "sins": sins,
-    }
+    archetypes = ["good cop", "bad cop"]
+
     if submit_button:
-        get_confession(
-            params,
-            callback_handler,
-        )
+        c1, c2 = st.columns(2)
+        with c1:
+            callback_handler1 = StreamHandler(container=st.empty())
+            good = get_confession(
+                params={
+                    "last_confession": last_confession,
+                    "sins": sins,
+                    "archetype": archetypes[0],
+                },
+                callback_handler=callback_handler1,
+            )
+        with c2:
+            callback_handler2 = StreamHandler(container=st.empty())
+            bad = get_confession(
+                params={
+                    "last_confession": last_confession,
+                    "sins": sins,
+                    "archetype": archetypes[1],
+                },
+                callback_handler=callback_handler2,
+            )
--- backend/writer.py
+++ backend/writer.py
@@ -4,9 +4,22 @@ from langchain.chat_models import ChatOpenAI
 
 PROMPT_TEMPLATE = """
 You are a catholic priest. A penitent has come to you for confession. 
-Be nice and moral, and a bit sarcasstic and funny.
+{archetype_description}
 
-Penitent confessed: {sins}. Last confession was: {last_confession}
+Penitent confessed: {sins}
+Last confession was: {last_confession}
+
+Bądź kreatywny w pokucie, nie dawaj samego ojcze nasz i zdrowaś Mario.
+
+Pokuta powinna polegać na karze, która przesyca grzech i skupia się na jego jeszcze większym popełnianiu,
+tak żeby spowiadany miał dosyć tego co uczynił, czyli np. 
+* jeśli ktoś zjadł czekoladę w Wielki Post, to niech przez tydzień je tylko czekoladę, aż do pożygania
+* jeżeli ktoś uprawiał seks pozamałżeński, to niech przez miesiąc uprawia seks 5 razy dziennie
+* jeżeli zapaliłeś fajkę to masz ich wypalić 10 na raz, żeby aż ci to zbrzydło
+
+Odpowiedz w języku polskim.
+
+Zacznij od słów: "Mój drogi synu, za twoje grzechy..."
 """
 
 
@@ -15,12 +28,12 @@ def get_confession(
     callback_handler: StreamlitCallbackHandler,
 ) -> str:
     summary_prompt_template = PromptTemplate(
-        input_variables=["sins", "last_confession"],
+        input_variables=["sins", "last_confession", "archetype_description"],
         template=PROMPT_TEMPLATE,
     )
 
     llm = ChatOpenAI(
-        temperature=0.0,
+        temperature=0.1,
         model_name="gpt-4o",
         streaming=True,
         callbacks=[callback_handler],
@@ -28,8 +41,23 @@ def get_confession(
 
     chain = LLMChain(llm=llm, prompt=summary_prompt_template, verbose=True)
 
+    archetype_description = ("You are a good cop type of individual."
+                             "You try to be moral, ethical, and honest in your actions."
+                             "You are a good listener and try to understand the other person's perspective."
+                             "You are empathetic and compassionate."
+                             "You are a good communicator and try to resolve conflicts peacefully."
+                             "Be like a good father"
+                             "") if params["archetype"] == "good cop" else ("You are a bad cop."
+                                                                            "You are tough, sarcastic and intimidating."
+                                                                            "You are not afraid to take risks and "
+                                                                            "make tough decisions."
+                                                                            "You are not afraid to confront people "
+                                                                            "and hold them accountable."
+                                                                            "Be like a bad cop")
+
     result = chain.run(
         sins=params["sins"],
-        last_confession=params["last_confession"]
+        last_confession=params["last_confession"],
+        archetype_description=archetype_description,
     )
     return result
